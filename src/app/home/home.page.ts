d396eac97995a0b5f77eaedabd2091b8426523e4
Marlon e Laura <134780074+MarlonPassos17@users.noreply.github.com>
2023-06-24 20:42:40 +0000

    contas_basicas

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,6 +11,27 @@ import { IonicModule } from '@ionic/angular';
   imports: [IonicModule, CommonModule, RouterLink],
 })
 export class HomePage {
-  resultado: number = 0;
+  resultado: string = "";
+  input: string = "";
 
+  ButtonNum(num: string){
+  this.input += num;
+  }
+
+  ButtonOperator(operator: string){
+  this.input += operator;
+  }
+
+  ButtonEqual(num: string){
+  this.resultado = eval(num);
+  }
+
+  ButtonClear(){
+  this.input = this.input.substr(0, this.input.length-1);
+  }
+
+  ButtonAllClear(){
+  this.input = '';
+  this.resultado ='';
+  }
 }
