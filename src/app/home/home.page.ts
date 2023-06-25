8a3695dfa134d1fddcc0dd1c7b5fc37dda8ca1b6
Marlon e Laura <134780074+MarlonPassos17@users.noreply.github.com>
2023-06-25 11:08:54 -0300

    Update home.page.ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,30 +8,66 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, RouterLink],
+  imports: [IonicModule, RouterLink, CommonModule],
 })
 export class HomePage {
   resultado: string = "";
   input: string = "";
+  operadores_vetor:string = '+-*/';
 
-  ButtonNum(num: string){
-  this.input += num;
+  ButtonNum(num: string) {
+    if (this.resultado.length > 0) {
+      this.resultado = '';
+    }
+    if (this.input.length == 0) {
+      if (num == '.' || num == '0') {
+        return;
+      }
+    }
+
+    this.input += num;
   }
 
-  ButtonOperator(operator: string){
-  this.input += operator;
+  ButtonOperator(operator: string) {
+    if (this.resultado.length > 0) {
+      this.input = this.resultado;
+      this.resultado = '';
+    }
+    if (this.input[this.input.length - 1] == '+' || this.input[this.input.length - 1] == '-' || this.input[this.input.length - 1] == '*' || this.input[this.input.length - 1] == '/' || this.input.length == 0) {
+      return;
+    }
+    this.input += operator;
   }
 
-  ButtonEqual(num: string){
-  this.resultado = eval(num);
+  ButtonEqual(num: string) {
+    this.resultado = eval(num).toFixed(2);
+    this.input = '';
   }
 
-  ButtonClear(){
-  this.input = this.input.substr(0, this.input.length-1);
+  ButtonClear() {
+    this.input = this.input.substr(0, this.input.length - 1);
   }
 
-  ButtonAllClear(){
-  this.input = '';
-  this.resultado ='';
+  ButtonAllClear() {
+    this.input = '';
+    this.resultado = '';
+  }
+
+  ButtonReverse() {
+    for (let i = 1; i != 0; i++) {
+      if (this.operadores_vetor.includes(this.input[this.input.length-i])) {
+      if (this.input[this.input.length-i] == '+') {
+        this.input[this.input.length-i].replace('+', '-');
+        console.log(this.input[this.input.length-i]); 
+      }
+      i = -1;
+      }
+    }
+  }
+
+  ButtonPorcentagem(){
+    this.input += '/100*'
+    // this.input += '%';
+    // this.input = this.input.replace('%', '/100*');
   }
 }
